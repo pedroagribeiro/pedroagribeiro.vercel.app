62dfde6da79fe64a582f179f902a87981e5b9358
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2024-07-17 23:35:09 +0100

    :sunrise:

---
+++ src/data/experience.ts
@@ -0,0 +1,39 @@
+import type { Props as ExperienceProp } from "../components/experience/props";
+
+export const EXPERIENCE: ExperienceProp[] = [
+  {
+    dates: "August 2022 - Present",
+    title: "Backend Developer",
+    company: "WithUs - Tecnhology Factory",
+    companyUrl: "https://www.withus.pt",
+    description:
+      "I am permanently assigned to AlticeLabs as a consultant. Designed and developed an application to automate the processing of alarmistic information using Java with the Spring Boot framework, Redis and PostgreSQL. Also helped design and implement two full fledged GoLang Backend applications using the Echo framework, Redis and PostgreSQL.",
+    technologies: [
+      "Java",
+      "Spring Boot",
+      "Redis",
+      "Echo",
+      "PostgreSQL",
+      "Docker",
+      "Kubernetes",
+    ],
+    logo: "/logos/withus.jpeg",
+  },
+  {
+    dates: "October 2021 — July 2022",
+    title: "Internship - Backend Developer",
+    company: "AlticeLabs",
+    companyUrl: "https://www.alticelabs.com/",
+    description:
+      "Worked on my master thesis regarding the management of queues for an in house software component in order to improve efficiency.",
+    technologies: [
+      "Java",
+      "Spring Boot",
+      "Redis",
+      "Docker",
+      "Kubernetes",
+      "RabbitMQ",
+    ],
+    logo: "/logos/alticelabs.jpeg",
+  },
+];
---
+++ src/data/socials.ts
@@ -0,0 +1,22 @@
+import IconDribbble from "../components/icons/dribbble.astro";
+import IconGithub from "../components/icons/github.astro";
+import IconLinkedin from "../components/icons/linkedin.astro";
+import IconTwitter from "../components/icons/twitter.astro";
+
+export const SOCIALS = [
+  {
+    name: "Github",
+    url: "https://github.com/pedroagribeiro",
+    icon: IconGithub,
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/pedroagribeiro/",
+    icon: IconLinkedin,
+  },
+  // {
+  //   name: "Twitter",
+  //   url: "https://twitter.com/pedroagribeiro",
+  //   icon: IconTwitter,
+  // },
+];
